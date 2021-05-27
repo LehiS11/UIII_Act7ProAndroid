5e45f615a5b08c75a6eb0360bc53b99c8327a5a2
FlutLab.io <>
2021-05-27 17:48:07 +0000

    Autosave

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -66,9 +66,14 @@ class _ThirdPageState extends State<ThirdPage> {
         ),
         InkWell(
           onTap: () {
-            print('Paquete');
+            print('Paquete 1');
           },
-          child: Container(padding: EdgeInsets.all(50), child: Image.asset("assets/images/img_menu.jpg")), //Fin Container 4
+          child: Container(padding: EdgeInsets.all(50), child: Image.asset("assets/images/img_menu.jpg")
+                         Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Burger Sencilla'),
+            )
+                          ), //Fin Container 4
         ),
       ] //Fin Widget[]
 
