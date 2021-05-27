8908179fb605171efb9fa61925a5b7a8dd5b3fbf
FlutLab.io <>
2021-05-27 16:50:01 +0000

    Autosave

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -6,11 +6,9 @@ class FirstPage extends StatelessWidget {
     return Container(
       child: Center(
         child: Text(
-          'Menu',
-          style: new TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black),
+          'First Page',
+          style: new TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.black),
         ), //Text
-      ), // center
-      child: Center(
         child: Image.asset("assets/images/menu.jpg"),
       ),
     ); //container
