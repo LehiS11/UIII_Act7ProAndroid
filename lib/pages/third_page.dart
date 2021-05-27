5611f26167c239029670e4d8e9d64d75847cbe5a
FlutLab.io <>
2021-05-27 17:44:42 +0000

    Autosave

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -69,23 +69,6 @@ class _ThirdPageState extends State<ThirdPage> {
             print('Que bien, sigamos');
           },
           child: Container(padding: EdgeInsets.all(50), child: Image.asset("assets/images/img_menu.jpg")), //Fin Container 4
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                ClipRRect(
-                  borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
-                  child: FadeInImage(
-                    placeholder: AssetImage('assets/burger.gif'),
-                    image: NetworkImage("https://cdn.dribbble.com/users/41854/screenshots/1121005/burger-d.gif"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Burger Sencilla'),
-                )
-              ],
-            ),
-          ),
         ),
       ] //Fin Widget[]
 
